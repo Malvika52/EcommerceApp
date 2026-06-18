580a6acc9a60027cd8847ffc084b6384c8953985
Malvika52 <56530699+Malvika52@users.noreply.github.com>
2026-06-18 15:26:04 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/ecommerceapp/screens/profile/SignupScreen.kt
@@ -0,0 +1,141 @@
+package com.example.ecommerceapp.screens.profile
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.ecommerceapp.viewmodels.AuthViewModel
+
+@Composable
+fun SignupScreen(
+    onNavigateToLogin: () -> Unit,
+    onSignUpSuccess: () -> Unit,
+    onSignUpError: () -> Unit,
+    authViewModel: AuthViewModel = hiltViewModel()
+) {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmpassword by remember { mutableStateOf("") }
+    var passwordError by remember { mutableStateOf<String?>(null) }
+
+
+    val authState = authViewModel.authState.collectAsState()
+
+    LaunchedEffect(authState.value) {
+        if (authState.value is AuthViewModel.AuthState.Success) {
+            onSignUpSuccess()
+        }
+    }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            "Create Account", style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(32.dp)
+        )
+
+        OutlinedTextField(
+            value = email, onValueChange = { email = it },
+            label = { Text("Enter your email") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp), singleLine = true
+        )
+
+        OutlinedTextField(
+            value = password, onValueChange = { password = it },
+            label = { Text("Enter your password") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp), singleLine = true
+        )
+
+        OutlinedTextField(
+            value = confirmpassword, onValueChange = { confirmpassword = it },
+            label = { Text("Enter your confirm password") },
+            visualTransformation = PasswordVisualTransformation(),
+            isError = passwordError != null,
+            supportingText = {
+                passwordError?.let {
+                    Text("Password Error", color = MaterialTheme.colorScheme.error)
+                }
+
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp), singleLine = true
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                Log.i("Malvika", "Button clicked")
+                if (password != confirmpassword) {
+                    Log.i("Malvika", "Button clicked 1")
+                    passwordError = "Password do not match"
+                } else if (password.length < 6) {
+                    passwordError = "Password length should be greater than 6"
+                    Log.i("Malvika", "Button clicked 2")
+                } else {
+                    passwordError = null
+                    authViewModel.signUp("User", email, password)
+                    Log.i("Malvika", "Button clicked 3")
+
+                }
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            enabled = email.isNotEmpty() && password.isNotEmpty() && confirmpassword.isNotEmpty()
+        ) {
+
+            Text("Sign up")
+            Spacer(Modifier.height(16.dp))
+
+
+            //If authstate is error
+
+        }
+    }
+
+}
