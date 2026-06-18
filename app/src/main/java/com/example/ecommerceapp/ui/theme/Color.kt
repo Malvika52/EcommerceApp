642b581086698ffa9bdfee18f1481b840c4b31c1
Malvika52 <56530699+Malvika52@users.noreply.github.com>
2026-06-18 15:20:06 +0530

    Add files via upload

--- app/src/main/java/com/example/ecommerceapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/ecommerceapp/ui/theme/Color.kt
@@ -1,1 +1,16 @@
+package com.example.ecommerceapp.ui.theme
 
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val PrimaryColor = Color(0xff6200ee)
+val CategoryChipColor = Color(0xFF1DEA41)
+val SandYellow = Color(0xFFFFC107)
+val DarkBlue = Color(0xFF03082A)
